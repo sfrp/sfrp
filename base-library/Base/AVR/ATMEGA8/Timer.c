84f6356adb34b1f0e8e6193eac111938fc3a8dd4
Kensuke Sawada <sasasawada@gmail.com>
2016-12-25 01:21:01 +0900

    [base-lib]refine timer lib for ATmega8

--- base-library/Base/AVR/ATMEGA8/Timer.c
+++ base-library/Base/AVR/ATMEGA8/Timer.c
@@ -1,29 +1,59 @@
 #include<avr/io.h>
 #include <avr/interrupt.h>
 
-static unsigned int elapsed_time_counter = 0;
+static unsigned int elapsed_clock_div256 = 0;
 
 ISR(TIMER0_OVF_vect)
 {
-  elapsed_time_counter++;
+  elapsed_clock_div256++;
 }
 
-void initialize() {
+void initialize_timer() {
+  static int flag = 0;
+  if (!flag) {
+    flag = 1;
+    return;
+  }
   cli();
   TIMSK = 0b00000001;
-  //TCCR0 = 0b00000011;
-  TCCR0 = 0b00000010;
+  TCCR0 = 0b00000001; // No prescaling
   sei();
 }
 
-int elapsed_time() {
-  static int initial = 1;
-  int res;
-  if (initial) {
-    initialize();
-    initial = 0;
-  }
-  res = elapsed_time_counter * 2;
-  elapsed_time_counter = 0;
-  return res;
+int get_dsec(clk_io_kilohertz) {
+  static unsigned int last = 0;
+  unsigned int current;
+  unsigned int dsec;
+  initialize_timer();
+
+  current = elapsed_clock_div256;
+  dsec = (current - last) / (clk_io_kilohertz * 4); // clk_io_kilohertz * 1024 / 256
+  last = current - (current - last) % (clk_io_kilohertz * 4);
+  return dsec;
+}
+
+int get_dmsec(clk_io_kilohertz) {
+  static unsigned int last = 0;
+  unsigned int current;
+  unsigned int dmsec;
+  initialize_timer();
+
+  current = elapsed_clock_div256;
+  dmsec = (current - last) / (clk_io_kilohertz / 2); // clk_io_kilohertz * 1024 / 256 / 8
+  last = current - (current - last) % (clk_io_kilohertz / 2);
+  return dmsec * 125;
+}
+
+// This function assumes 0.97656(=1000/1024)msec as 1msec
+// so accumulates about 2% error.
+int get_uncertain_dmsec(clk_io_kilohertz) {
+  static unsigned int last = 0;
+  unsigned int current;
+  unsigned int dmsec;
+  initialize_timer();
+
+  current = elapsed_clock_div256;
+  dmsec = (current - last) / (clk_io_kilohertz / 256); // clk_io_kilohertz * 1024 / 256 / 1024
+  last = current - (current - last) % (clk_io_kilohertz / 256);
+  return dmsec;
 }
--- base-library/Base/AVR/ATMEGA8/Timer.h
+++ base-library/Base/AVR/ATMEGA8/Timer.h
@@ -1,6 +1,8 @@
 #ifndef BASE_IO_AVR_ATMEGA8_TIMER
 #define BASE_IO_AVR_ATMEGA8_TIMER
 
-int elapsed_time();
+int get_dsec(int);
+int get_dmsec(int);
+int get_uncertain_dmsec(int);
 
 #endif
